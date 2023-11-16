9d90e80d0e8ff202f57b35e201f360524f804d23
MiguelMSandin <miguelmendezsandin@gmail.com>
2023-11-16 22:15:06 +0100

    Delete scripts/1_phylo directory

--- scripts/1_phylo/A6.1_phylo_mrBayes.sh
+++
@@ -1,8 +0,0 @@
-
-set autoclose=yes nowarnings=yes
-execute all_filtered_align-linsi_trim05.nexus
-lset nst=6 rates=gamma
-mcmc ngen=10000000 Nruns=4 savebrlens=yes file=all_filtered_align-linsi_trim05_mrBayes
-sump
-sumt
-quit
--- scripts/1_phylo/A6_phylo_mrBayes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mb < "A6.1_phylo_mrBayes.sh" > "all_filtered_align-linsi_trim05_mrBayes.log"
-
